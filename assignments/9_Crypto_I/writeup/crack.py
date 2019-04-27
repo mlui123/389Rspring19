596b7a386643e7c610b496e6c542a78e21de3f8a
Mitchell Lui <mitchlui3@gmail.com>
2019-04-27 17:29:43 -0400

    finished part1 of crypto1

--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -4,16 +4,29 @@ import hashlib
 import string
 
 def crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    # open and read hashes.txt
+    with open("hashes.txt", "r") as f:
+        hashes = f.read().splitlines()
+
+   # open and read passwords.txt
+    with open("passwords.txt", "r") as f2:
+        passwords = f2.read().splitlines()
+
     characters = string.ascii_lowercase
 
     for c in characters:
         for p in passwords:
             # crack hashes
+            input = c + p
+            temp_hash = hashlib.sha256((input).encode()).hexdigest()
 
             # print hashes as 'input:hash'
             # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
+
+            if temp_hash in hashes:
+                print(input + ":" + temp_hash)
+
+            
 
 if __name__ == "__main__":
     crack()
--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -6,11 +6,19 @@ import socket
 import time
 
 def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+
+    # open and read hashes.txt
+    with open("hashes.txt", "r") as f:
+        hashes = f.read().splitlines()
+
+   # open and read passwords.txt
+    with open("passwords.txt", "r") as f2:
+        passwords = f2.read().splitlines()
+
+
     characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
-    server_port = 00000
+    server_ip = '134.209.128.58 1337'
+    server_port = 1337
 
     s = socket.socket(socket.AF_INET, socket.STREAM)
     s.connect((server_ip, server_port))
