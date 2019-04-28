64152b604946e8e71354ed5f09cd988a9f4c7d77
Mitchell Lui <mitchlui3@gmail.com>
2019-04-28 00:17:13 -0400

    done with assignment 9

--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -6,25 +6,40 @@ import socket
 import time
 
 def server_crack():
-
     # open and read hashes.txt
-    with open("hashes.txt", "r") as f:
-        hashes = f.read().splitlines()
-
-   # open and read passwords.txt
+    hashes = {}
+    # open and read passwords.txt
     with open("passwords.txt", "r") as f2:
         passwords = f2.read().splitlines()
 
-
     characters = string.ascii_lowercase
-    server_ip = '134.209.128.58 1337'
+    server_ip = '134.209.128.58'
     server_port = 1337
 
-    s = socket.socket(socket.AF_INET, socket.STREAM)
+    for c in characters:
+        for p in passwords:
+            input = c + p
+            temp_hash = hashlib.sha256((input).encode()).hexdigest()
+            hashes[temp_hash] = input
+            
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
-    data = s.recv(1024)
+    
     # parse data
     # crack 3 times
+
+    for i in range(0,3):
+        data = s.recv(1024).decode()
+        hash = data.split('\n')[2]
+        print("input: " + hash + ", cracked: " + hashes[hash])
+
+        s.send((hashes[hash] + "\n").encode())
+        time.sleep(.1)
+
+    flag = s.recv(1024)
+    print(flag.decode())
+            
 
 if __name__ == "__main__":
     server_crack()
